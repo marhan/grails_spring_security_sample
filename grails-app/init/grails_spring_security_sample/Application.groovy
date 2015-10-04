dcd02c714627bc534df6da5bee2280559c4e37dd
Markus Hanses <me@markushanses.de>
2015-10-04 21:27:34 +0200

    Initial commit with bare Grails 3.0.8.

---
+++ grails-app/init/grails_spring_security_sample/Application.groovy
@@ -0,0 +1,10 @@
+package grails_spring_security_sample
+
+import grails.boot.GrailsApp
+import grails.boot.config.GrailsAutoConfiguration
+
+class Application extends GrailsAutoConfiguration {
+    static void main(String[] args) {
+        GrailsApp.run(Application, args)
+    }
+}
